dcf0c708e3a0155fb2ad27918c71b2f6a18dba31
wadva <wadva474@gmail.com>
2022-10-01 09:40:47 +0100

    chore : setups dependency injection(Hilt)

--- app/src/main/java/com/wadud/user/MainActivity.kt
+++ app/src/main/java/com/wadud/user/MainActivity.kt
@@ -11,33 +11,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.wadud.user.ui.theme.UserTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             UserTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     UserTheme {
-        Greeting("Android")
+
     }
 }
